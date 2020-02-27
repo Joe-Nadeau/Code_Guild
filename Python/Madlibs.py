b66581b2172fa10b6aafe87704113b4aaad888de
Joseph Nadeau <josephnadeau@Nicolesy.localdomain>
2020-02-26 20:22:46 -0800

    Madlib

---
+++ Python/Madlibs.py
@@ -0,0 +1,14 @@
+noun_1 = input('enter a plural noun: ')
+noun_2 = input('enter another noun: ')
+verb_1 = input('enter a verb: ')
+verb_2 = input('enter another verb: ')
+verb_3 = input('enter another verb: ')
+adjective_1 = input('enter an adjective: ')
+
+print(f'This is my madlib: We\'ve been {verb_1}ing most our {noun_1} living in a {noun_2}\'s paradise. We don\'t {verb_2}, we all {verb_3} {adjective_1}, living in a {noun_2} paradise. ')
+
+
+
+# We've been spending most our lives living in a (noun)
+# paradise. We don't (verb), we all play (adjective), 
+# living in a (noun) paradise.
