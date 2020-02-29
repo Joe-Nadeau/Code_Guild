1cd599205a079b98a8b3ac7dcc08d4d84bae5a91
Joseph <jnadeau133@gmail.com>
2020-02-28 19:28:50 -0800

    Lab 3 Version 2 complete! :poop:

--- Python/grading.py
+++ Python/grading.py
@@ -5,17 +5,43 @@
 print('\nThis program will convert your numerical grade into a letter grade.')
 
 num_grade = input('Enter your numerical grade: ')
-num_grade = int(num_grade)
-if num_grade > 100:
+num_grade = float(num_grade)
+if num_grade > 110:
     print('Way to go Einstein! ')
+elif num_grade >= 100.01 and num_grade <= 110:
+    print('Wow, you got an A+! You must be really smart or...what\'s that on your nose?')
 elif num_grade >= 90 and num_grade <= 100:
-    print('Your grade is an A ')
+    if num_grade % 10 > 5: 
+        qualifier = '+'
+    elif num_grade % 10 == 5:
+        qualifier = ''
+    elif num_grade % 10 < 5:
+        qualifier = '-'
+    print('Your grade is an: A' + qualifier)
 elif num_grade >= 80 and num_grade <= 89:
-    print('Your grade is a B ')
+    if num_grade % 10 > 5: 
+        qualifier = '+'
+    elif num_grade % 10 == 5:
+        qualifier = ''
+    elif num_grade % 10 < 5:
+        qualifier = '-'
+    print('Your grade is a B ' + qualifier)
 elif num_grade >= 70 and num_grade <= 79:
-    print('Your grade is a C ')
+    if num_grade % 10 > 5: 
+        qualifier = '+'
+    elif num_grade % 10 == 5:
+        qualifier = ''
+    elif num_grade % 10 < 5:
+        qualifier = '-'
+    print('Your grade is a C ' + qualifier)
 elif num_grade >= 60 and num_grade <= 69:
-    print('Your grade is a D ')
-elif num_grade <= 59:
+    if num_grade % 10 > 5: 
+        qualifier = '+'
+    elif num_grade % 10 == 5:
+        qualifier = ''
+    elif num_grade % 10 < 5:
+        qualifier = '-'
+    print('Your grade is a D ' + qualifier)
+elif num_grade < 60:
     print('You better try harder bud. ')
 
