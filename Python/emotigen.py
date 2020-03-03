ed9e3a0701a15a7eb3832b9650367b281d940b58
Joseph <jnadeau133@gmail.com>
2020-03-02 20:04:08 -0800

    Lab 5: Emoticon Generator_Version 2 :art:

--- Python/emotigen.py
+++ Python/emotigen.py
@@ -7,6 +7,10 @@ import random
 eyeses = ['B', '8', ':', ';', 'X']
 noses = ['C', '3', 'o','0', 'O', '>', '<']
 mouths = ['D', '(', ')', ']', '[', 'O', 'P', 'b']
+x = 0
 
-print(random.choice(eyeses) + random.choice(noses) + random.choice(mouths))
+while x < 5:
+    
+    print(random.choice(eyeses) + random.choice(noses) + random.choice(mouths) )
+    x += 1
 
