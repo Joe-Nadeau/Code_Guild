07688324763bb96d056c68959e8737df58de2cc2
Joseph <jnadeau133@gmail.com>
2020-03-02 19:28:51 -0800

    Lab 5: Random Emoticon Generator :poop:

---
+++ Python/emotigen.py
@@ -0,0 +1,12 @@
+# PDX Code Guild
+# Lab 5
+# Emoticon generator
+
+import random
+
+eyeses = ['B', '8', ':', ';', 'X']
+noses = ['C', '3', 'o','0', 'O', '>', '<']
+mouths = ['D', '(', ')', ']', '[', 'O', 'P', 'b']
+
+print(random.choice(eyeses) + random.choice(noses) + random.choice(mouths))
+
