aed12fee7868d59b4ed70cb1bc2c3a2d681ca6a2
Joseph <jnadeau133@gmail.com>
2020-03-03 19:56:09 -0800

    Improving password complexity :lock:

--- Python/passwordgen.py
+++ Python/passwordgen.py
@@ -9,11 +9,17 @@ lowercase_letter = "abcdefghijklmnopqrstuvwxyz"
 uppercase_letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 special_character = '!@#$%^&*'
-rand_selecter = [random.choice(lowercase_letter), random.choice(uppercase_letter), random.choice(number), random.choice(special_character)]
 
 x = 0
+new_password =[]
 
 while x < 12:
+    rand_selecter = [random.choice(lowercase_letter), random.choice(uppercase_letter), random.choice(number), random.choice(special_character)]
+    # character = random.choice(rand_selecter)
+    # rand_selecter.remove(character)
+    # new_password.append(character)
     print(random.choice(rand_selecter), end='')
+
     x += 1
 
+# print("".join(rand_selecter))
