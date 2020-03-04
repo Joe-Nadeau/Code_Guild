3d2f8d455620f24dfa384534cede005a0ff8187a
Joseph <jnadeau133@gmail.com>
2020-03-03 20:06:30 -0800

    Password generator version 2 :construction:

--- Python/passwordgen.py
+++ Python/passwordgen.py
@@ -10,10 +10,12 @@ uppercase_letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 special_character = '!@#$%^&*'
 
+pass_length = input('Enter desired number of characters for you password: ')
+pass_length = int(pass_length)
 x = 0
 new_password =[]
 
-while x < 12:
+while x < pass_length:
     rand_selecter = [random.choice(lowercase_letter), random.choice(uppercase_letter), random.choice(number), random.choice(special_character)]
     # character = random.choice(rand_selecter)
     # rand_selecter.remove(character)
