1f61afbb40e8b3f61338e67e6ffcdfea420682d9
Joseph <jnadeau133@gmail.com>
2020-03-04 20:16:57 -0800

    Lab 7 Rock Paper Scissors :tada:

---
+++ Python/ropasc.py
@@ -0,0 +1,37 @@
+# PDX Code Guild
+# Lab 7
+# Rock Paper Scissors
+
+import random
+
+def winner():
+
+    weapon = ['rock', 'paper', 'scissors']
+    cpu_weapon = random.choice(weapon)
+    user_weapon = input('Choose your weapon: \nrock, \npaper, \nor \nscissors \n')
+
+    if cpu_weapon == 'rock' and user_weapon == 'rock':
+        print('It\'s a tie!')
+    elif cpu_weapon == 'rock' and user_weapon == 'paper':
+        print('You win, I lose!')
+    elif cpu_weapon == 'rock' and user_weapon == 'scissors':
+        print('I win, you lose!')
+    elif cpu_weapon == 'paper' and user_weapon == 'paper':
+        print('It\'s a tie!')
+    elif cpu_weapon == 'paper' and user_weapon == 'scissors':
+        print('You win, I lose!')
+    elif cpu_weapon == 'paper' and user_weapon == 'rock':
+        print('I win, you lose!')
+    elif cpu_weapon == 'scissors' and user_weapon == 'scissors':
+        print('It\'s a tie!')
+    elif cpu_weapon == 'scissors' and user_weapon == 'rock':
+        print('You win, I lose!')
+    elif cpu_weapon == 'scissors' and user_weapon == 'paper':
+        print('I win, you lose!')
+    
+def main():
+
+    winner()
+
+main()
+        
