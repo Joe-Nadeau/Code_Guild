8276541b3d6970a108390794b605f0c32c54ea74
Joseph <jnadeau133@gmail.com>
2020-03-05 17:31:50 -0800

    Making my code look better :art:

--- Python/ropasc.py
+++ Python/ropasc.py
@@ -10,24 +10,24 @@ def winner():
     cpu_weapon = random.choice(weapon)
     user_weapon = input('Choose your weapon: \nrock, \npaper, \nor \nscissors \n')
 
-    if cpu_weapon == 'rock' and user_weapon == 'rock':
+    if cpu_weapon == user_weapon:
         print('It\'s a tie!')
-    elif cpu_weapon == 'rock' and user_weapon == 'paper':
-        print('You win, I lose!')
-    elif cpu_weapon == 'rock' and user_weapon == 'scissors':
-        print('I win, you lose!')
-    elif cpu_weapon == 'paper' and user_weapon == 'paper':
-        print('It\'s a tie!')
-    elif cpu_weapon == 'paper' and user_weapon == 'scissors':
-        print('You win, I lose!')
-    elif cpu_weapon == 'paper' and user_weapon == 'rock':
-        print('I win, you lose!')
-    elif cpu_weapon == 'scissors' and user_weapon == 'scissors':
-        print('It\'s a tie!')
-    elif cpu_weapon == 'scissors' and user_weapon == 'rock':
-        print('You win, I lose!')
-    elif cpu_weapon == 'scissors' and user_weapon == 'paper':
-        print('I win, you lose!')
+    if cpu_weapon == 'rock':
+        if user_weapon == 'paper':
+            print('You win, I lose!')
+        if user_weapon == 'scissors':
+            print('I win, you lose!')
+    if cpu_weapon == 'paper':
+        if user_weapon == 'scissors':
+            print('You win, I lose!')
+        if user_weapon == 'rock':
+            print('I win, you lose!')
+    if cpu_weapon == 'scissors': 
+        if user_weapon == 'rock':
+            print('You win, I lose!')
+    if cpu_weapon == 'scissors':
+        if user_weapon == 'paper':
+            print('I win, you lose!')
     
 def main():
 
@@ -35,7 +35,7 @@ def main():
         winner()
         play_again = input('Would you like to play again?: <y/n>')
         if play_again == 'n':
-            print('Fine! I thought humans were supposed to be fun. Are you sure you\'re not a computer?')
+            print('\n Fine! I thought humans were supposed to be fun. Are you sure you\'re not a computer?\n ')
             break        
 main()
         
