79e4411da87ccc0959fbe33e6592267cf694d2fb
Joseph <jnadeau133@gmail.com>
2020-03-09 19:40:53 -0700

    Lab 10: Average numbers, Version 2 *forgot to get average in Version 1

--- Python/avgnums.py
+++ Python/avgnums.py
@@ -1,12 +1,21 @@
 def main():
+    nums = []
+    def user_numlist():
+            
+        while True:
+            user_input = input('Enter a number or type \'done\': ')
+            if user_input == 'done':
+                break
+            nums.append(int(user_input))
 
-    nums = [5, 0, 8, 3, 4, 1, 6]
+    user_numlist()
     numsum = []
     x = 0
     for i in nums:
         x += i
         numsum.append(x)
         print(numsum)
-    print(f'The sum of the numbers in the list is:{numsum[6:]}')
+    print(f'The sum of the numbers in the list is:{numsum[len(numsum)-1]}')
+    print(f'The average of the numbers in the list is: {numsum[len(numsum)-1] / len(numsum)}')
 
 main()
