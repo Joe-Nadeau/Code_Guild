d02ade126f1b9be3033de0841d206b60f7062132
Joseph <jnadeau133@gmail.com>
2020-03-13 19:18:39 -0700

    Lab 13: ROT-13, Version 1 is working smooth as budda

--- Python/ROT13.py
+++ Python/ROT13.py
@@ -8,7 +8,8 @@ import string
 def main():
 
     user_string = input("Write a message that you wan't encrypted with a cypher: ")
-    alphabet = string.ascii_letters
+    lower_case = string.ascii_lowercase
+    upper_case = string.ascii_uppercase
     #rot_13_var = 'nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM'
     cyphered_string = ''
     cy_list = []
@@ -18,11 +19,17 @@ def main():
         #for each character in the user's string
         for char in user_string:
 
-            if char in alphabet:
+            if char in lower_case:
                 #looks for the character from the user's string and matches it to the corresponding character in ascii_letters. Then it stores that letter's indice in the variable to_be_replaced and adds 13 to that indice
-                to_be_replaced = (alphabet.find(char) + 13) % 52
+                to_be_replaced = (lower_case.find(char) + 13) % 26
                 #looks at the indice stored in to_be_replaced, matches it to the same indice in rot_13_var and adds the character associated with that indice to cy_list.
-                cy_list.append(alphabet[to_be_replaced])
+                cy_list.append(lower_case[to_be_replaced])
+            
+            elif char in upper_case:
+                
+                to_be_replaced = (upper_case.find(char) + 13) % 26
+
+                cy_list.append(upper_case[to_be_replaced])
 
             else:
                 cy_list.append(char)
