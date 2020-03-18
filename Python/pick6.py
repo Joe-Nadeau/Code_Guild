b70298864609a471e68a4c2a63be08f748ff3314
Joseph <jnadeau133@gmail.com>
2020-03-17 19:24:32 -0700

    Attempting version 2

--- Python/pick6.py
+++ Python/pick6.py
@@ -14,6 +14,8 @@
 # Add to your balance the winnings from your matches
 # After the loop, print the final balance
 
+#Version 2
+
 import random
 
 def pick6():
@@ -27,34 +29,43 @@ def num_matches(winner, ticket):
     matches = 0
 
     for i in range(len(winner)):
-        print(i)
         
         if ticket[i] == winner[i]:
             matches += 1
-            print(matches)
     return matches
+    print(f'The number of matches is: {matches}')
 
 def balance_tracker(matches):
     balance = 0
+    winnings = 0
 
     if matches == 0:
         balance = balance - 2
     elif matches == 1:
         balance = (balance + 4) - 2
+        winnings = (winnings + 4)
     elif matches == 2:
         balance = (balance + 7)- 2
+        winnings = (winnings + 7)
     elif matches == 3:
         balance = (balance + 100) - 2
+        winnings = (winnings + 100)
     elif matches ==4:
         balance = (balance + 50000) - 2
+        winnings = (winnings + 50000)
     elif matches == 5:
         balance = (balance + 1000000) - 2
+        winnings = (winnings + 1000000)
     elif matches == 6:
         balance = (balance + 25000000) - 2
-    return balance
+        winnings = (winnings +)
+    return balance, winnings
 
+def ROI():
+    
 
 def main():
+
     final_balance = 0
     x = 0
     winner = pick6()
@@ -65,10 +76,11 @@ def main():
         final_balance = final_balance + balance_tracker(num_matches(winner, ticket))
         print(f'The winning numbers are: {winner}')
         print(f'Your ticket numbers were: {ticket}')
-        print(num_matches(winner, ticket))
-        print(final_balance)
+        print(f'The number of matches in this round is: {num_matches(winner, ticket)}')
+        print(f'The running balance is: {final_balance}\n')
         x += 1
 
     print(f'Your final balance is: ${final_balance}')
+    print(f'Your return on investment is{}')
 
 main()
