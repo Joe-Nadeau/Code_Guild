923c0565f5e95fc964a2ac1caa1a36e428e9d9fb
Joseph <jnadeau133@gmail.com>
2020-03-18 19:44:52 -0700

    Lab 15: Version 2 :construction:

--- Python/numbertophrase.py
+++ Python/numbertophrase.py
@@ -20,14 +20,19 @@ def main():
             ones_place = user_number % 10
             out_num = [tens_dict.get(tens_place),ones_dict.get(ones_place)]
             return "- ".join(out_num)
-            
+        elif user_number in range(100, 1000):
+            hundreds_place = (user_number // 100) * 100
+            tens_place = (user_number // 10) * 10
+            ones_place = user_number % 10
+            out_num = [hundreds_dict.get(hundreds_place), tens_dict.get(tens_place), ones_dict.get(ones_place)]
+            return "- ".join(out_num)
 
     user_number = int(input('Enter a 1 or 2 digit number with the number keys: '))
 
     ones_dict = {0: "zero", 1: "one ", 2: "two ", 3: "three ", 4: "four ", 5: "five ", 6: "six ", 7: "seven ", 8: "eight ", 9: "nine "}
     teens_dict = {10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"} 
     tens_dict = {20: "twenty ", 30: "thirty ", 40: "forty ", 50: "fifty ", 60: "sixty ", 70: "seventy ", 80: "eighty ", 90: "ninety "}
-
+    hundreds_dict = {100: "one hundred", 200: "two hundred", 300: "three hundred", 400: "four hundred", 500: "five hundred", 600: "six hundred", 700: "seven hundred", 800: "eight hundred", 900: "nine hundred"}
     print(f'The number/s you entered is/are spelled out like this:{num_transmuter(user_number)}')
 
 main()
