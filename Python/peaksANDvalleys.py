3a4962497b6e787bf8d0c24a502d40ac7a671241
Joseph <jnadeau133@gmail.com>
2020-03-20 21:20:05 -0700

    Lab 18: Peaks nd Valleys, Version 1. Comments

--- Python/peaksANDvalleys.py
+++ Python/peaksANDvalleys.py
@@ -4,10 +4,23 @@
 #Joey Nadeau
 
 def peaks(data):
-
-
+        
+        peaks = []
+        # creates a list of indices that is equal to the length(or number of items) in the list named 'data'
+        indices  = list(range(len(data))) 
+        # for each item in the list named 'indices' do the following:
+        for index in indices:
+            # if there is an index in the position before the specified index and there is an idex in the position after the specified index, then do the following:
+            if (index - 1) in indices and (index + 1) in indices:
+                # if a certain index in the list named 'data' is greater than or equal to the index in the prior position AND it is greater than or equal to the index in the following position, then do the following:
+                if data[index] is >= data[index - 1] and data[index] >= data[index + 1]:
+                    print(f'{index} is a peak')
+                    peaks.apend[index]
+                continue
+        return peaks
 def valleys(data):
-
+    
+    valleys = []
 
 def peaks_and_valleys(data):
 
@@ -15,3 +28,5 @@ def peaks_and_valleys(data):
 def main():
 
     data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+
+main()
