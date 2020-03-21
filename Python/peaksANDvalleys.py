5b73be99003293d5897d06049bb9c432009823cc
Joseph <jnadeau133@gmail.com>
2020-03-20 19:51:55 -0700

    Lab 18: Peaks and Valleys, Version 1 :tada:

---
+++ Python/peaksANDvalleys.py
@@ -0,0 +1,17 @@
+#PDX Code Guild
+#Lab 18
+#Peaks and Valleys
+#Joey Nadeau
+
+def peaks(data):
+
+
+def valleys(data):
+
+
+def peaks_and_valleys(data):
+
+
+def main():
+
+    data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
