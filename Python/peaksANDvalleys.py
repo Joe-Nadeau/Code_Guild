2e6824640e19b3655f7f1af02ab772a98e28afb6
Joseph <jnadeau133@gmail.com>
2020-03-23 19:30:02 -0700

    Lab 18: Peaks and Valleys, V1 complete

--- Python/peaksANDvalleys.py
+++ Python/peaksANDvalleys.py
@@ -13,11 +13,11 @@ def peaks(data):
             # if there is an index in the position before the specified index and there is an idex in the position after the specified index, then do the following:
             if (index - 1) in indices and (index + 1) in indices:
                 # if a certain index in the list named 'data' is greater than or equal to the index in the prior position AND it is greater than or equal to the index in the following position, then do the following:
-                if data[index] is >= data[index - 1] and data[index] >= data[index + 1]:
+                if data[index] >= data[index - 1] and data[index] >= data[index + 1]:
                     # prints the index specified in line 16 and labels it a 'peak'
-                    print(f'{index} is a peak')
+                    print(f'{data[index]} is a peak')
                     # adds the index specified in line 16 to the list 'peaks'
-                    peaks.apend[index]
+                    peaks.append(data[index])
                 # repeats the for loop for the next item in the list 'indices'
                 continue
         # returns the appended list 'peaks'
@@ -29,19 +29,35 @@ def valleys(data):
     indices  = list(range(len(data)))
     for index in indices:
         if (index - 1) in indices and (index + 1) in indices:
-            if data[index] is >= data[index - 1] and data[index] >= data[index + 1]:
-                print(f'{index} is a valley')
-                valleys.apend[index]
+            if data[index] <= data[index - 1] and data[index] <= data[index + 1]:
+                print(f'{data[index]} is a valley')
+                valleys.append(data[index])
             continue
     return valleys
 
 def peaks_and_valleys(data):
 
+    pks_vlys = []
+    indices = list(range(len(data)))
+    for index in indices:
+        if(index - 1) in indices and (index + 1) in indices:
+            if data[index] >= data[index -1] and data[index] >= data[index +1]:
+                pks_vlys.append(data[index])
+            elif data[index] <= data[index -1] and data[index] <= data[index +1]:
+                pks_vlys.append(data[index])
+            continue
+    return pks_vlys
+
 
 def main():
 
     data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+    print(f'\nData set: {data}\n')
     print(peaks(data))
+    print("_________________")
     print(valleys(data))
+    print('_________________')
+    print(f'Peaks and Valleys in order of apperance: {peaks_and_valleys(data)}\n')
+    
 
 main()
