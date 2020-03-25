af0bc584d0f46de1e06293d78c05a9f9a7f2a459
Joseph <jnadeau133@gmail.com>
2020-03-24 20:05:37 -0700

    Lab 19: Black Jack advice, Version 2 COMPLETE!

--- Python/blackjackadv.py
+++ Python/blackjackadv.py
@@ -4,7 +4,7 @@
 #Joey Nadeau
 
 
-def hand_total(user_hand_list, card_values):
+def hand_total_func(user_hand_list, card_values):
     
     sum = 0
     
@@ -17,6 +17,12 @@ def hand_total(user_hand_list, card_values):
                 sum = value + sum
     return sum
 
+def Aces(hand_total):
+
+    if hand_total + 11 > 21:
+        return 1
+    else:
+        return 11
 
 def main():
 
@@ -24,12 +30,14 @@ def main():
 
     user_hand = (input('Enter 3 cards separated by commas.\n A = Ace\n K = King\n Q = Queen\n J = Jack\n For the number cards just type the number:\n'))
     user_hand_list = user_hand.split(',')
+    
     print(user_hand_list)
+
     card_values = {
-                    'A': 1,
+                    'A': 0,
                     'K': 10,
                     'Q': 10,
-                    'J':10,
+                    'J': 10,
                     '10': 10,
                     '9': 9,
                     '8': 8,
@@ -41,15 +49,27 @@ def main():
                     '2': 2
                     }
 
-    print(f'Your hand total is: {hand_total(user_hand_list, card_values)}')
+    hand_total = hand_total_func(user_hand_list, card_values)
 
-    if hand_total(user_hand_list, card_values) < 17:
+
+    if 'A' in user_hand_list:
+        Ace_value = Aces(hand_total)
+        print(hand_total + Ace_value)
+        if hand_total + Ace_value < 17:
+            print("I advise that you: hit")
+        elif hand_total + Ace_value >= 17 and hand_total + Ace_value < 21:
+            print("I advise that you: stay")
+        elif hand_total + Ace_value == 21:
+            print("I advise that you: yell BLACKJACK!!!")
+        elif hand_total + Ace_value > 21:
+            print("You are BUSTED and I advise that you: quit before you get in too deep.")
+    elif hand_total < 17:
         print("I advise that you: hit")
-    elif hand_total(user_hand_list, card_values) >= 17 and hand_total(user_hand_list, card_values) < 21:
+    elif hand_total >= 17 and hand_total < 21:
         print("I advise that you: stay")
-    elif hand_total(user_hand_list, card_values) == 21:
+    elif hand_total == 21:
         print("I advise that you: yell BLACKJACK!!!")
-    elif hand_total(user_hand_list, card_values) > 21:
+    elif hand_total > 21:
         print("You are BUSTED and I advise that you: quit before you get in too deep.")
 
 main()
