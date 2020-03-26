2d8607bbf8ab2b2f547e8dfbabd7d883b3a848d5
Joseph <jnadeau133@gmail.com>
2020-03-25 21:31:57 -0700

    Lab 20: Credit Card Validation, COMPLETE

--- Python/ccval.py
+++ Python/ccval.py
@@ -6,23 +6,34 @@
 
 def main():
 
-CC_number = input("Enter the 16 digit credit card number that you would like to validate: \n")
-CC_numstring = list(map(int, CC_number))
-print(CC_numstring)
-CC_numstring = CC_numstring[:-1]
-print(CC_numstring)
-CC_numstring = CC_numstring[::-1]
-print(CC_numstring)
+    CC_number = input("Enter the 16 digit credit card number that you would like to validate: \n")
+    CC_numstring = list(map(int, CC_number))
+    print(CC_numstring)
 
-for i in range(1, len(CC_numstring), 2):
-    CC_numstring[i] *= 2
-print(f'{CC_numstring}\n')
+    check_number = CC_numstring.pop()
+    print(check_number)
 
-for i in CC_numstring:
-    if i > 9:
-        x = CC_numstring.index(i)
-        CC_numstring[x] = i - 9
-print(CC_numstring)
+    CC_numstring = CC_numstring[::-1]
+    print(CC_numstring)
+
+    for i in range(0, len(CC_numstring), 2):
+        CC_numstring[i] *= 2
+    print(f'{CC_numstring}\n')
+
+    for i in CC_numstring:
+        if i > 9:
+            x = CC_numstring.index(i)
+            CC_numstring[x] = i - 9
+    print(CC_numstring)
+
+    total = sum(CC_numstring)
+    print(total)
+    print(total % 10)
+
+    if (total % 10)  == check_number:
+        print('The card number is valid.')
+    else:
+        print('The card number is NOT valid.')
 
 main()
 
