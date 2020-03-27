a2f4e4754842c98073a8e1d7fcccaf4f49d93ede
Joseph <jnadeau133@gmail.com>
2020-03-26 20:09:31 -0700

    Lab 21: Count Words, Version 1 :tada:

---
+++ Python/countwords.py
@@ -0,0 +1,9 @@
+# PDX Code Guild
+# Lab 21
+# Count Words
+# Joey Nadeau
+
+Shelley_Frankenstein = open("Frankenstein.txt", "r")
+contents = Shelley_Frankenstein.read()
+print(contents)
+Shelley_Frankenstein.close()
