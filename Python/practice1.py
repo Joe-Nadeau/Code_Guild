5cbcc1b910d07a6b1f02dd7efcff8eba2b2e8534
Joseph <jnadeau133@gmail.com>
2020-03-26 21:46:17 -0700

    Practice problems

---
+++ Python/practice1.py
@@ -0,0 +1,37 @@
+# PDX Code Guild
+# Practice 1: Fundamentals
+# Joey Nadeau
+
+# Problem 1
+
+def is_even(x):
+
+    if x % 2 == 0:
+        return True
+    elif x % 2 != 0:
+        return False
+
+#print(is_even(7))
+#print(is_even(8))
+
+# Problem 2
+
+def opposite(a, b):
+
+    if a < 0 and b >= 0:
+        return True
+    elif a >= 0 and b < 0:
+        return True
+    else:
+        return False
+
+# print(opposite(-1, 2))
+# print(opposite(2, -8))
+# print(opposite(2, 5))
+# print(opposite(-1, -3))
+
+# Problem 3
+
+def near_100(num):
+
+    if 
