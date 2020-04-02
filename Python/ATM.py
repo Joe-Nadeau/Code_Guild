046d334af0fc1c2519cc459766e1cd8cd0aec9ec
Joseph <jnadeau133@gmail.com>
2020-04-01 18:13:40 -0700

    Lab 25, ATM :tada:

---
+++ Python/ATM.py
@@ -0,0 +1,17 @@
+# PDX Code Guild
+# Lab 25
+# ATM
+# Joey Nadeau
+
+class ATM:
+    def __init__(self, balance, interest_rate)
+        self.balance = balance
+        self.interest_rate = interest_rate
+    
+    def get_balance(self):
+        return f'Your balance is: {self.balance}'
+
+    def get_interest(self):
+        return f'Your interest rate is: {self.interest_rate}'
+        
+
