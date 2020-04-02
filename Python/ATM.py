8b1a4a913d02726f1f55a3be17f567090bea87c3
Joseph <jnadeau133@gmail.com>
2020-04-01 18:42:20 -0700

    Lab 25, Version 1 :construction:

--- Python/ATM.py
+++ Python/ATM.py
@@ -8,10 +8,21 @@ class ATM:
         self.balance = balance
         self.interest_rate = interest_rate
     
-    def get_balance(self):
+    # returns the account balance
+    def check_balance(self):
         return f'Your balance is: {self.balance}'
 
-    def get_interest(self):
-        return f'Your interest rate is: {self.interest_rate}'
-        
+    # Get amount from user, add it to balance
+    def deposit(amount):
+        return f'You have deposited ${amount} into your account.\n Your new balance is ${balance}.'
 
+    # returns true if the withdrawn amount won't put the account in the negative
+    def check_withdrawal(amount):
+        return True
+    # withdraws the amount from the account and returns it
+    def withdraw(amount):
+        return 
+
+    #returns the amount of interest calculated on the account
+    def calc_interest(self):
+        return f'Your interest rate is: {self.interest_rate}'
