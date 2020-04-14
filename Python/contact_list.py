b54f1d5989791e0671151ceb8fe19c76b61bc592
Joseph <jnadeau133@gmail.com>
2020-04-13 20:16:35 -0700

    A whole bunch of stuff

--- Python/contact_list.py
+++ Python/contact_list.py
@@ -3,6 +3,30 @@
 # Contact List
 # Joey Nadeau
 
-with open('contacts.csv', 'r') as file:
-    lines = file.read().split('\n')
-    print(lines)
+import csv
+
+def new_contact(contacts):
+    
+    name = input("Enter a name: \n")
+    fruit = input("Enter a fruit: \n")
+    color = input("Enter a color: \n")
+    new_contact = {name: name, favorite_fruit: fruit, favorite_color: color}
+    new_contact.append(name)
+    new_contact.append(fruit)
+    new_contact.append(color)
+    contacts.append(new_contact)
+    return contacts
+
+
+def main():
+
+    with open('contacts.csv', 'r') as file:
+        #lines = file.read().split('\n')
+        reader = csv.reader(file, skipinitialspace = True)
+        header = next(reader)
+        contacts = [dict(zip(header, map(str, row))) for row in reader]
+        #new_contact(contacts)
+        print(contacts)
+
+main()
+
