20986fe9e3eaa35d200390049af4ca64dfe0efe1
Joey Nadeau <49260652+Dantes-Shade@users.noreply.github.com>
2020-04-29 21:26:13 -0700

    ToDo App :tada:

---
+++ Django/todolist/todo_app/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class TodoItem(models.Model):
+    task_description = models.TextField(blank = False, null = True)
+    created_date = models.DateTimeField(default = timezone.now)
+    completion_date = models.DateTimeField()
+    is_completed = models.BooleanField(default = False)
+
+    def complete_task(self):
+        self.is_completed = True
+        self.completion_date = timezone.now()
+        return self.is_completed
