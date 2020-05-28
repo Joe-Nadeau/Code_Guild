f603eb8daba8d3e267687aab59c98dfc23912e1d
Joey Nadeau <49260652+Dantes-Shade@users.noreply.github.com>
2020-05-27 21:43:38 -0700

    Javascript Unit Converter :tada:

---
+++ Javascript/unit_converter.js
@@ -0,0 +1,10 @@
+const readline = require('readline-sync');
+
+function ft_converter(user_num) {
+    const ft_out = (a * 0.3048)
+    return ft_out
+}
+
+user_num = prompt("Enter a number in feet that you want converted to meters:");
+
+console.log(ft_converter(a))
