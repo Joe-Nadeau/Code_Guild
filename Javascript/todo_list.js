e77effc2b5ea26f4631498a8900ea08abbcbb118
Joey Nadeau <49260652+Dantes-Shade@users.noreply.github.com>
2020-06-02 21:30:06 -0700

    Javascript Todo list lab, complete

--- Javascript/todo_list.js
+++ Javascript/todo_list.js
@@ -1,12 +1,31 @@
 let btn = document.querySelector("#newItemButton")
 let input = document.querySelector("#itemText")
 let ul = document.querySelector("#theList")
+let btnRemove = document.querySelector("#removeItems")
 
 btn.addEventListener("click", function(){
     let li = document.createElement("li");
     let linode = document.createTextNode(input.value)
     li.appendChild(linode)
-    console.log(li);
-    console.log(li.innerText)
     ul.insertBefore(li, ul.childNodes[0]);
 })
+
+btn.addEventListener("click", function(){
+    let all_li = document.querySelectorAll("li")
+    for (let li of all_li){
+        li.addEventListener("click", function(){
+            li.classList.toggle("mark")
+        })
+    }
+})
+
+btnRemove.addEventListener("click", function(){
+    let all_li = document.querySelectorAll("li")
+    for (let i=0; i < all_li.length; i++){
+        if (all_li[i].classList.contains("mark")){
+            all_li[i].remove()
+        }
+    }
+})
+
+
