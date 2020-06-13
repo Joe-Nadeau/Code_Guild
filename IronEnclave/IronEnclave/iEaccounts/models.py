244fcbb814ba523efaa9d4c13761fb9b57ceafa2
Joey Nadeau <49260652+Dantes-Shade@users.noreply.github.com>
2020-06-12 21:08:48 -0700

    Adding models and new apps

--- IronEnclave/IronEnclave/iEaccounts/models.py
+++ IronEnclave/IronEnclave/iEaccounts/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+import datetime
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Users(models.Model):
+    pass
+
+class Profiles(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null = True, blank = True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    profile_img = models.ImageField(upload_to='profile')
+    bio = models.CharField(max_length=10000)
+    weight_class = models.CharField(max_length=3)
---
+++ IronEnclave/IronEnclave/iEgraph/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IegraphConfig(AppConfig):
+    name = 'iEgraph'
